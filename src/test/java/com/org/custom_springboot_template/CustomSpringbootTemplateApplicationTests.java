d267b101b4053f9ea0dbbc638efbd1b615433b3e
caglar <eflatun126@gmail.com>
2022-06-04 19:03:08 +0300

    Initial commit

---
+++ src/test/java/com/org/custom_springboot_template/CustomSpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.custom_springboot_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomSpringbootTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
